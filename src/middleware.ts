e0001004a8469cad6432fa57098cd090d2cb7640
Nepal Highland Treks <nepalhighlandtreks2080@gmail.com>
2025-09-29 09:03:18 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'ir

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getIronSession } from 'iron-session/edge';
+import { getIronSession } from 'iron-session';
 import { sessionOptions } from './lib/auth';
 
 export async function middleware(request: NextRequest) {
